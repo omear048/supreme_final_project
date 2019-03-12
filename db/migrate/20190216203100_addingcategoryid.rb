e97b5ec9cde45c6267625fd95504ffc4a0be77bd
Kris OMeara <omear048@gmail.com>
2019-03-11 22:06:47 -0600

    Changed subcategoty table to use :integer for category

--- db/migrate/20190216203100_addingcategoryid.rb
+++ db/migrate/20190216203100_addingcategoryid.rb
@@ -1,5 +1,5 @@
 class Addingcategoryid < ActiveRecord::Migration[5.2]
   def change
-    add_column :subcategories, :category_id, :number
+    add_column :subcategories, :category_id, :integer
   end
 end
